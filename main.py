f340fc682f9aa7430e8b60188db94dee9fe7f6b7
LiuChang24 <166979356+LiuChang24@users.noreply.github.com>
2024-04-14 15:44:44 +0800

    PCA+M

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
-from mlxtend.plotting import plot_decision_regions
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 from classifier import MahalanobisClassifier
 from sklearn.metrics import classification_report, accuracy_score, roc_auc_score, confusion_matrix
 
-# 读取数据
+# Read Dataset
 df = pd.read_csv('https://raw.githubusercontent.com/jbrownlee/Datasets/master/wheat-seeds.csv', header=None)
 df.dropna(inplace=True)  # Drop missing values.
 df.head()
 
-# 数据集划分
+# split the dataset
 X = df.iloc[:, 0:7].values  # Sample Data
 y = df.iloc[:, 7].values  # Label Data
 unique_y = np.unique(y)  # To return the correct labels when predicting
@@ -23,37 +21,36 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratif
 # split the dataset in 70:30 ratio as Train and Test
 
 # Normalization
-sc = StandardScaler()
+sc = StandardScaler()  # instantiate
 X_train_std = sc.fit_transform(X_train)
-X_test_std = sc.transform(X_test)
+X_test_std = sc.transform(X_test)  # Normalize the dataset
 
-# LDA
-lda = LDA(n_components=2)
-X_train_lda = lda.fit_transform(X_train_std, y_train)
-X_test_lda = lda.fit_transform(X_test_std, y_test)
-lr = LogisticRegression()
-lr = lr.fit(X_train_lda, y_train)
+# PCA
+pca = PCA(n_components=2) # 压缩到二维特征
+X_train_pca = pca.fit_transform(X_train_std) # 对训练数据进行处理
+X_test_pca = pca.transform(X_test_std) # 对测试集数据进行处理
 
-plot_decision_regions(X_train_lda, y_train, lr)
-plt.xlabel('LD 1')
-plt.ylabel('LD 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-plt.show()
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
 
-plot_decision_regions(X_test_lda, y_test, lr)
-plt.xlabel('LD 1')
-plt.ylabel('LD 2')
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    # 按照样本的真实值进行展示
+    plt.scatter(X_train_pca[y_train == l, 0],
+                X_train_pca[y_train == l, 1],
+                c=c, label=l, marker=m)
+
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
 plt.legend(loc='lower left')
 plt.tight_layout()
 plt.show()
 
 # "Training"
-clf = MahalanobisClassifier(X_train_lda, y_train)
+clf = MahalanobisClassifier(X_train_pca, y_train)
 
 # Predicting
-pred_probs = clf.predict_probability(X_test_lda)
-pred_class = clf.predict_class(X_test_lda,unique_y)
+pred_probs = clf.predict_probability(X_test_pca)
+pred_class = clf.predict_class(X_test_pca,unique_y)
 
 pred_actuals = pd.DataFrame([(pred, act) for pred, act in zip(pred_class, y_test)], columns=['pred', 'true'])
 print(pred_actuals[:5])
@@ -63,3 +60,4 @@ pred = pred_actuals.loc[:, 'pred']
 scores = np.array(pred_probs)[:, 1]
 print('\nAccuracy Score: ', accuracy_score(truth, pred))
 print('\nClassification Report: \n', classification_report(truth, pred))
+
